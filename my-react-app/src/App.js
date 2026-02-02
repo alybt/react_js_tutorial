7978b9603e804376ad8a006b5989db40a54249ba
alybt <alelybitun.school@gmail.com>
2026-02-02 19:07:24 +0800

    Create Variables

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,8 +2,13 @@ import HelloWorld from "./components/HelloWorld";
 
 
 function App() {
+  const saygoodbye = "See you again";
+  const number = 15;
+
+
   return ( <div className = "App">
     <HelloWorld/>
+    <div>{saygoodbye} {number + number}</div>
      </div> );
 }
 
--- my-react-app/src/components/HelloWorld.js
+++ my-react-app/src/components/HelloWorld.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 
 function HelloWorld(){
+
+    const greeting = "Hello World";
+    const introduction = "My name is Aly";
+    const link = "http://www.google.com"
+
     return(
-        <div>HelloWorld</div>
+        <div>
+            <div>{greeting}</div>
+            <div>{introduction}</div>
+            <a href ={link}> Go to Google</a>
+        </div>
     )
 }
 
