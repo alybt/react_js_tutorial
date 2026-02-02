ddd061f32bb8f5a5a4d1762f0b0c924992585b08
alybt <alelybitun.school@gmail.com>
2026-02-02 20:30:58 +0800

    List and Map

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -8,9 +8,14 @@ import Navbar from "./components/Navbar";
 function App() {
   
   const [name, setName] = useState("");
+  
+  const [students, setStudents] = useState(['Alex', 'David', 'Joe'])
+ 
+  
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert("My name is" + name);
+    setStudents(currentStudents => [...currentStudents, name ]);
+    setName('');
   }
 
   return ( 
@@ -18,10 +23,18 @@ function App() {
       <Navbar></Navbar> 
       <br></br>
       <form onSubmit={handleSubmit}>
-        <div>What's your name?</div>
+        <div>Add new Student </div>
         <input type= "text" value={name} onChange={(e) => setName(e.target.value)}/>
         <input type="submit"/>
       </form>
+
+      <br></br>
+      <h1>All Students</h1>
+      <ul>
+        {
+          students.map((students, index) => <li key={index}>{students}</li>)
+        }
+      </ul>
 
 
     </div> );
