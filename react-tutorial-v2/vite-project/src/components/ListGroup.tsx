ced3094781013387a777179ff8b82f00e8d5cde3
alybt <alelybitun.school@gmail.com>
2026-02-19 15:58:24 +0800

    v2: Conditional Formatting v1

--- react-tutorial-v2/vite-project/src/components/ListGroup.tsx
+++ react-tutorial-v2/vite-project/src/components/ListGroup.tsx
@@ -1,6 +1,16 @@
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+  items = [];
+
+  if (items.length === 0)
+    return (
+      <>
+        <h1>Lists</h1>
+        <p>No Item Found.</p>
+      </>
+    );
+
   return (
     <>
       <h1>List</h1>
