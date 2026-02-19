b07f3af7219d812fb939028d4a3a22df92e5b5ea
alybt <alelybitun.school@gmail.com>
2026-02-19 16:03:43 +0800

    v2: Element

--- react-tutorial-v2/vite-project/src/components/ListGroup.tsx
+++ react-tutorial-v2/vite-project/src/components/ListGroup.tsx
@@ -2,14 +2,11 @@ function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
-  const getMessage = () => {
-    items.length === 0 ? <p>No item found. </p> : null;
-  };
-
   return (
     <>
       <h1>List</h1>
-      {getMessage()}
+      {items.length === 0 ? <p>No item found. </p> : null}
+
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
