667459fa93c91ecb58b183dbbe7c3578264969ba
alybt <alelybitun.school@gmail.com>
2026-02-19 16:26:21 +0800

    v2: Event handler

--- react-tutorial-v2/vite-project/src/components/ListGroup.tsx
+++ react-tutorial-v2/vite-project/src/components/ListGroup.tsx
@@ -1,14 +1,20 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+
+  //Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No Item Found.</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" onClick={handleClick} key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
